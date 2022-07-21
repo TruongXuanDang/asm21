2b1a6824947fadf7fb02d45d4fd9bec0bd77c6df
truonghusk17 <truonghusk17@gmail.com>
2022-07-22 06:56:15 +0700

    INTEGRATION: Insert pages

---
+++ src/main/java/com/example/demo/controllers/MockController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MockController {
+	@RequestMapping("/guestbook")
+	public String viewGuestbookPage(Model model) {
+		return "pages/guestbook";
+	}
+	
+	@RequestMapping("/partyAddress")
+	public String viewPartyAddressPage(Model model) {
+		return "pages/partyAddress";
+	}
+	
+	@RequestMapping("/photoAlbum")
+	public String viewPhotoAlbumPage(Model model) {
+		return "pages/photoAlbum";
+	}
+	
+	@RequestMapping("/weddingBlog")
+	public String viewWeddingBlogPage(Model model) {
+		return "pages/weddingBlog";
+	}
+	
+	@RequestMapping("/wishlist")
+	public String viewWishlistPage(Model model) {
+		return "pages/wishlist";
+	}
+}
