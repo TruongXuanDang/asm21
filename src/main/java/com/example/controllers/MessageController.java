3bc7722f75d604ddc7c6e7a0a10f97194cff6822
truonghusk17 <truonghusk17@gmail.com>
2022-07-28 22:15:27 +0700

    INTEGRATION: Add guest book page

--- src/main/java/com/example/controllers/MessageController.java
+++ src/main/java/com/example/controllers/MessageController.java
@@ -1,5 +1,38 @@
 package com.example.controllers;
 
-public class MessageController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Product;
+import com.example.entities.Entities;
+import com.example.entities.Messages;
+import com.example.services.EntityService;
+import com.example.services.MessageService;
+
+@Controller
+public class MessageController {
+	@Autowired
+	private MessageService service;
+
+	@RequestMapping("/guest-book")
+	public String viewGuuestBook(Model model) {
+		List<Messages> listMessages = service.listAll();
+		Messages message = new Messages();
+		model.addAttribute("listMessages", listMessages);
+		model.addAttribute("messages", message);
+		System.out.print(listMessages);
+		return "pages/guestbook";
+	}
+	
+	@RequestMapping(value="/saveMessage", method= RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("messages") Messages message) {
+		service.save(message);
+		return "redirect:/guest-book";
+	}
 }
--- src/main/java/com/example/services/MessageService.java
+++ src/main/java/com/example/services/MessageService.java
@@ -1,5 +1,26 @@
 package com.example.services;
 
-public class MessageService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Product;
+import com.example.entities.Entities;
+import com.example.entities.Messages;
+import com.example.repositories.EntityRepository;
+import com.example.repositories.MessageRepository;
+
+@Service
+public class MessageService {
+	@Autowired
+	private MessageRepository repo;
+	
+	public List<Messages> listAll() {
+		return repo.findAll();
+	}
+	
+	public void save(Messages message) {
+		repo.save(message);
+	}
 }
