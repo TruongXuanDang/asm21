41d672cb156f94a8f79d79f60678896cc08e1d97
truonghusk17 <truonghusk17@gmail.com>
2022-07-30 16:47:34 +0700

    INTEGRATION: Change Wedding Blog page

--- src/main/java/com/example/controllers/MockController.java
+++ src/main/java/com/example/controllers/MockController.java
@@ -6,28 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MockController {
-	@RequestMapping("/guestbook")
-	public String viewGuestbookPage(Model model) {
-		return "pages/guestbook";
-	}
-	
-	@RequestMapping("/partyAddress")
+	@RequestMapping("/party-address")
 	public String viewPartyAddressPage(Model model) {
 		return "pages/partyAddress";
-	}
-	
-	@RequestMapping("/photoAlbum")
-	public String viewPhotoAlbumPage(Model model) {
-		return "pages/photoAlbum";
-	}
-	
-	@RequestMapping("/weddingBlog")
-	public String viewWeddingBlogPage(Model model) {
-		return "pages/weddingBlog";
-	}
-	
-	@RequestMapping("/wishlist")
-	public String viewWishlistPage(Model model) {
-		return "pages/wishlist";
 	}
 }
--- src/main/java/com/example/controllers/PhotoController.java
+++ src/main/java/com/example/controllers/PhotoController.java
@@ -1,5 +1,13 @@
 package com.example.controllers;
 
-public class PhotoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class PhotoController {
+	@RequestMapping("/photo-album")
+	public String viewPhotoAlbumPage(Model model) {
+		return "pages/photoAlbum";
+	}
 }
